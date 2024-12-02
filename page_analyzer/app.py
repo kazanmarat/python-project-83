d54bac2934577f11bb99da59447229f78668df25
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2024-12-02 08:29:50 +0300

    move dotenv package to dev dependencies and change import load_dotenv function

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 import os
-from dotenv import load_dotenv
 
 
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ModuleNotFoundError:
+    pass
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
